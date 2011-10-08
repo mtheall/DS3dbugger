c3690dba39d44f7cc84a6aaa84d2c9f83bee35ba
zeromus <zeromus@zeromus.org>
2011-10-08 02:22:20 -0500

    Revert "so what? are there problems here?"
    This reverts commit 07fd776e7b1aefed62a18fe16f738b1ca8c56c1a.

--- PC/MainForm.Designer.cs
+++ PC/MainForm.Designer.cs
@@ -28,130 +28,96 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.btnConnect = new System.Windows.Forms.Button();
-            this.txtHost = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.btnAcquireScreen = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnCopyScreen = new System.Windows.Forms.Button();
-            this.txtFilename = new System.Windows.Forms.TextBox();
-            this.btnChooseFile = new System.Windows.Forms.Button();
-            this.btnSend = new System.Windows.Forms.Button();
-            this.vpDSScreen = new RetainedViewportPanel();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // btnConnect
-            // 
-            this.btnConnect.Location = new System.Drawing.Point(827, 11);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(75, 23);
-            this.btnConnect.TabIndex = 0;
-            this.btnConnect.Text = "Connect";
-            this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
-            // 
-            // txtHost
-            // 
-            this.txtHost.Location = new System.Drawing.Point(540, 13);
-            this.txtHost.Name = "txtHost";
-            this.txtHost.Size = new System.Drawing.Size(281, 20);
-            this.txtHost.TabIndex = 1;
-            // 
-            // panel1
-            // 
-            this.panel1.AutoSize = true;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.vpDSScreen);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(522, 394);
-            this.panel1.TabIndex = 3;
-            // 
-            // btnAcquireScreen
-            // 
-            this.btnAcquireScreen.Location = new System.Drawing.Point(93, 412);
-            this.btnAcquireScreen.Name = "btnAcquireScreen";
-            this.btnAcquireScreen.Size = new System.Drawing.Size(63, 23);
-            this.btnAcquireScreen.TabIndex = 4;
-            this.btnAcquireScreen.Text = "Acquire";
-            this.btnAcquireScreen.UseVisualStyleBackColor = true;
-            this.btnAcquireScreen.Click += new System.EventHandler(this.btnAcquireScreen_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 417);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "NDS 3d @ 2x";
-            // 
-            // btnCopyScreen
-            // 
-            this.btnCopyScreen.Location = new System.Drawing.Point(162, 412);
-            this.btnCopyScreen.Name = "btnCopyScreen";
-            this.btnCopyScreen.Size = new System.Drawing.Size(63, 23);
-            this.btnCopyScreen.TabIndex = 6;
-            this.btnCopyScreen.Text = "Copy";
-            this.btnCopyScreen.UseVisualStyleBackColor = true;
-            this.btnCopyScreen.Click += new System.EventHandler(this.btnCopyScreen_Click);
-            // 
-            // txtFilename
-            // 
-            this.txtFilename.Location = new System.Drawing.Point(540, 42);
-            this.txtFilename.Name = "txtFilename";
-            this.txtFilename.ReadOnly = true;
-            this.txtFilename.Size = new System.Drawing.Size(281, 20);
-            this.txtFilename.TabIndex = 7;
-            // 
-            // btnChooseFile
-            // 
-            this.btnChooseFile.Location = new System.Drawing.Point(827, 40);
-            this.btnChooseFile.Name = "btnChooseFile";
-            this.btnChooseFile.Size = new System.Drawing.Size(75, 23);
-            this.btnChooseFile.TabIndex = 8;
-            this.btnChooseFile.Text = "Choose File";
-            this.btnChooseFile.UseVisualStyleBackColor = true;
-            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
-            // 
-            // btnSend
-            // 
-            this.btnSend.Location = new System.Drawing.Point(827, 69);
-            this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.TabIndex = 9;
-            this.btnSend.Text = "Send";
-            this.btnSend.UseVisualStyleBackColor = true;
-            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
-            // 
-            // vpDSScreen
-            // 
-            this.vpDSScreen.Location = new System.Drawing.Point(3, 3);
-            this.vpDSScreen.Name = "vpDSScreen";
-            this.vpDSScreen.Size = new System.Drawing.Size(512, 384);
-            this.vpDSScreen.TabIndex = 2;
-            this.vpDSScreen.Text = "label1";
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 479);
-            this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.btnChooseFile);
-            this.Controls.Add(this.txtFilename);
-            this.Controls.Add(this.btnCopyScreen);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnAcquireScreen);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.txtHost);
-            this.Controls.Add(this.btnConnect);
-            this.Name = "MainForm";
-            this.Text = "MainForm";
-            this.panel1.ResumeLayout(false);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.txtHost = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnAcquireScreen = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.vpDSScreen = new RetainedViewportPanel();
+			this.btnCopyScreen = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Location = new System.Drawing.Point(830, 12);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(75, 23);
+			this.btnConnect.TabIndex = 0;
+			this.btnConnect.Text = "Connect";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+			// 
+			// txtHost
+			// 
+			this.txtHost.Location = new System.Drawing.Point(724, 12);
+			this.txtHost.Name = "txtHost";
+			this.txtHost.Size = new System.Drawing.Size(100, 20);
+			this.txtHost.TabIndex = 1;
+			// 
+			// panel1
+			// 
+			this.panel1.AutoSize = true;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.vpDSScreen);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(522, 394);
+			this.panel1.TabIndex = 3;
+			// 
+			// btnAcquireScreen
+			// 
+			this.btnAcquireScreen.Location = new System.Drawing.Point(93, 412);
+			this.btnAcquireScreen.Name = "btnAcquireScreen";
+			this.btnAcquireScreen.Size = new System.Drawing.Size(63, 23);
+			this.btnAcquireScreen.TabIndex = 4;
+			this.btnAcquireScreen.Text = "Acquire";
+			this.btnAcquireScreen.UseVisualStyleBackColor = true;
+			this.btnAcquireScreen.Click += new System.EventHandler(this.btnAcquireScreen_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(14, 417);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(73, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "NDS 3d @ 2x";
+			// 
+			// vpDSScreen
+			// 
+			this.vpDSScreen.Location = new System.Drawing.Point(3, 3);
+			this.vpDSScreen.Name = "vpDSScreen";
+			this.vpDSScreen.Size = new System.Drawing.Size(512, 384);
+			this.vpDSScreen.TabIndex = 2;
+			this.vpDSScreen.Text = "label1";
+			// 
+			// btnCopyScreen
+			// 
+			this.btnCopyScreen.Location = new System.Drawing.Point(162, 412);
+			this.btnCopyScreen.Name = "btnCopyScreen";
+			this.btnCopyScreen.Size = new System.Drawing.Size(63, 23);
+			this.btnCopyScreen.TabIndex = 6;
+			this.btnCopyScreen.Text = "Copy";
+			this.btnCopyScreen.UseVisualStyleBackColor = true;
+			this.btnCopyScreen.Click += new System.EventHandler(this.btnCopyScreen_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(917, 479);
+			this.Controls.Add(this.btnCopyScreen);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnAcquireScreen);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.txtHost);
+			this.Controls.Add(this.btnConnect);
+			this.Name = "MainForm";
+			this.Text = "MainForm";
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -164,8 +130,5 @@
 		private System.Windows.Forms.Button btnAcquireScreen;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button btnCopyScreen;
-        private System.Windows.Forms.TextBox txtFilename;
-        private System.Windows.Forms.Button btnChooseFile;
-        private System.Windows.Forms.Button btnSend;
 	}
 }
--- PC/MainForm.cs
+++ PC/MainForm.cs
@@ -59,87 +59,75 @@ namespace DS3dbugger
 			ConfigService.Save(fnPrefs, Config);
 		}
 
-        uint[] testData = new uint[] {
-		0x15101210,
-                    0x00000002, // MTX_MODE Position & Vector Simultaneous Set mode
-                    0x00000000, // MTX_POP 0
-                    0x00000002, // MTX_MODE Position & Vector Simultaneous Set mode
-                                // MTX_IDENTITY
-        0x15101510,
-                    0x00000000, // MTX_MODE Projection
-                                // MTX_IDENTITY
-                    0x00000003, // MTX_MODE Texture
-                                // MTX_IDENTITY
-        0x10292A50,
-                    0x00000000, // SWAP_BUFFERS Auto-sort, Z-value
-                    0x00000000, // TEXIMAGE_PARAM
-                    0x00000000, // POLYGON_ATTR
-                    0x00000000, // MTX_MODE Projection
-        0x10151015,
-                                // MTX_IDENTITY
-                    0x00000002, // MTX_MODE Position & Vector Simultaneous Set mode
-                                // MTX_IDENTITY
-                    0x00000003, // MTX_MODE Texture
-        0x15106015,
-                                // MTX_IDENTITY
-                    0xBFFF0000, // VIEWPORT (0, 0, 255, 191)
-                    0x00000000, // MTX_MODE Projection
-                                // MTX_IDENTITY
-        0x29201800,
-                                // NOP
-                    0x00001127, // MTX_MULT_4x4 1.072021484
-                    0x00000000, //              0.0
-                    0x00000000, //              0.0
-                    0x00000000, //              0.0
-                    0x00000000, //              0.0
-                    0x000016E1, //              1.429931641
-                    0x00000000, //              0.0
-                    0x00000000, //              0.0
-                    0xFFFFFFFB, //             -0.001220703
-                    0x00000000, //              0.0
-                    0xFFFFEFF8, //             -1.001953125
-                    0xFFFFF000, //             -1.0
-                    0x00000000, //              0.0
-                    0x00000000, //              0.0
-                    0xFFFFFCCE, //             -0.199707031
-                    0x00000000, //              0.0
-                    0x0000001F, // COLOR RGB15(31, 0, 0)
-                    0x001F00C0, // POLYGON_ATTR Back Surface Render, Front Surface Render, Alpha 31
-        0x1C151110,
-                    0x00000002, // MTX_MODE Position & Vector Simultaneous Set mode
-                                // MTX_PUSH
-                                // MTX_IDENTITY
-                    0xFFFFE800, // MTX_TRANS X = -1.5
-                    0x00000000, //           Y =  0.0
-                    0xFFFFA000, //           Z = -6.0
-        0x23232340,
-                    0x00000000, // BEGIN_VTXS Triangles
-                    0x10000000, // VTX_16 X =  1.0, Y =  0.0
-                    0x00000000, //        Z =  0.0
-                    0xF000F000, // VTX_16 X = -1.0, Y = -1.0
-                    0x00000000, //        Z =  0.0
-                    0xF0001000, // VTX_16 X = -1.0, Y =  1.0
-                    0x00000000, //        Z =  0.0
-        0x40201C41,
-                                // END_VTXS
-                    0x00003000, // MTX_TRANS X = 3.0
-                    0x00000000, //           Y = 0.0
-                    0x00000000, //           Z = 0.0
-                    0x000003E0, // COLOR RGB15(0, 31, 0)
-                    0x00000001, // BEGIN_VTXS Quads
-        0x23232323,
-                    0x1000F000, // VTX_16 X =  1.0, Y = -1.0
-                    0x00000000, //        Z =  0.0
-                    0x10001000, // VTX_16 X =  1.0, Y =  1.0
-                    0x00000000, //        Z =  0.0
-                    0xF0001000, // VTX_16 X = -1.0, Y =  1.0
-                    0x00000000, //        Z =  0.0
-                    0xF000F000, // VTX_16 X = -1.0, Y = -1.0
-                    0x00000000, //        Z =  0.0
-        0x00501241,
-                                // END_VTXS
-                    0x00000001, // MTX_POP 1
-                    0x00000000, // SWAP_BUFFERS Auto-sort, Z-value
+		uint[] testData = new uint[] {
+		0x00000010, 0x00000002,
+0x00000012, 0x00000000,
+0x00000010, 0x00000002,
+0x00000015,
+0x00000010, 0x00000000,
+0x00000015,
+0x00000010, 0x00000003,
+0x00000015,
+0x00000050, 0x00000000,
+0x0000002A, 0x00000000,
+0x00000029, 0x00000000,
+0x00000010, 0x00000000,
+0x00000015,
+0x00000010, 0x00000002,
+0x00000015,
+0x00000010, 0x00000003,
+0x00000015,
+0x00000060, 0xBFFF0000,
+0x00000010, 0x00000000,
+0x00000015, 0x00000000,
+0x00000018, 0x00001127,
+0x00000000,
+0x00000000,
+0x00000000,
+0x00000000,
+0x000016E1,
+0x00000000,
+0x00000000,
+0xFFFFFFFB,
+0x00000000,
+0xFFFFEFF8,
+0xFFFFF000,
+0x00000000,
+0x00000000,
+0xFFFFFCCE,
+0x00000000,
+0x00000020, 0x0000001F,
+0x00000029, 0x001F00C0,
+0x00000010, 0x00000002,
+0x00000011,
+0x00000015,
+0x0000001C, 0xFFFFE800,
+0x00000000,
+0xFFFFA000,
+0x00000040, 0x00000000,
+0x00000023, 0x10000000,
+0x00000000,
+0x00000023, 0xF000F000,
+0x00000000,
+0x00000023, 0xF0001000,
+0x00000000,
+0x00000041,
+0x0000001C, 0x00003000,
+0x00000000,
+0x00000000,
+0x00000020, 0x000003E0,
+0x00000040, 0x00000001,
+0x00000023, 0x1000F000,
+0x00000000,
+0x00000023, 0x10001000,
+0x00000000,
+0x00000023, 0xF0001000,
+0x00000000,
+0x00000023, 0xF000F000,
+0x00000000,
+0x00000041,
+0x00000012, 0x00000001,
+0x00000050, 0x00000000,
 		};
 
 		Bitmap lastScreen;
@@ -160,6 +148,8 @@ namespace DS3dbugger
 			inf.SetInput(zipbuf);
 			byte[] bscreen = new byte[256 * 192 * 2];
 			inf.Inflate(bscreen);
+
+			int zzz = 9;
 
 			var bmp = new Bitmap(256, 192, PixelFormat.Format32bppArgb);
 			BitmapData bmpdata = bmp.LockBits(new System.Drawing.Rectangle(0, 0, 256, 192), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
@@ -204,7 +194,6 @@ namespace DS3dbugger
 
 			btnConnect.Enabled = false;
 			btnConnect.Text = "connected";
-            txtHost.ReadOnly = true;
 
 			msg.type = MessageType.Message_DisplayList;
 			msg.displist_size = testData.Length * 4 + 4;
@@ -231,40 +220,6 @@ namespace DS3dbugger
 		{
 			System.Windows.Forms.Clipboard.SetImage(lastScreen);
 		}
-
-        private void btnChooseFile_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Filter = "All files (*.*)|*.*";
-            dialog.Title = "Select a file";
-            if (dialog.ShowDialog() == DialogResult.OK)
-                txtFilename.Text = dialog.FileName;
-        }
-
-        private void btnSend_Click(object sender, EventArgs e)
-        {
-            if (txtFilename.Text != "")
-            {
-                int length = (int)(new FileInfo(txtFilename.Text)).Length;
-                byte[] buffer = new byte[length];
-
-                BinaryReader fr = new BinaryReader(File.Open(txtFilename.Text, FileMode.Open));
-                fr.Read(buffer, 0, length);
-                fr.Close();
-
-                BinaryWriter bw = new BinaryWriter(ns);
-                Message msg = new Message();
-                msg.type = MessageType.Message_DisplayList;
-                msg.displist_size = length + 4;
-                msg.Send(ns);
-
-                bw.Write(length);
-                bw.Write(buffer, 0, length);
-
-                bw.Flush();
-                ns.Flush();
-            }
-        }
 	}
 
 	class ConfigStruct
